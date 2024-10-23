ef84a738ae1ace892de9e6a29a222a6c41ef930f
Farhan Ibrahim <farhan5543@gmail.com>
2024-10-23 12:43:27 +0800

    refactor: UI

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:order_bot_app/models.dart';
 import 'package:order_bot_app/ui/Bot.dart';
 import 'package:order_bot_app/ui/controls.dart';
+import 'package:order_bot_app/ui/list.dart';
 
 class HomeWidget extends StatefulWidget {
   const HomeWidget({super.key});
@@ -30,7 +31,68 @@ class _HomeWidgetState extends State<HomeWidget> {
     });
   }
 
-  void _addOrderToBot(Bot bot, Order order) {
+  void _addNewOrder(OrderType type) {
+    setState(() {
+      final newOrder = Order(id: _orderCounter, type: type);
+      _incrementOrderCounter();
+      _queue.insert(
+          newOrder.type == OrderType.vip ? 0 : _queue.length, newOrder);
+
+      final availableBot = _bots.firstWhereOrNull(
+        (bot) => bot.status == BotStatus.idle,
+      );
+      if (availableBot != null) {
+        _processOrder(availableBot, newOrder);
+      }
+    });
+  }
+
+  void _onCompleteOrder(Order order, Bot bot) {
+    setState(() {
+      _completedOrders.add(order);
+      if (_queue.isNotEmpty) {
+        _processOrder(bot, _queue.first);
+      }
+    });
+  }
+
+  void _addBot() {
+    setState(() {
+      final newBot = Bot(id: _botCounter);
+      _incrementBotCounter();
+      final nextOrder = _queue
+          .firstWhereOrNull((order) => order.status == OrderStatus.pending);
+      if (nextOrder != null) {
+        _processOrder(newBot, nextOrder);
+      }
+      _bots.add(newBot);
+    });
+  }
+
+  void _removeBot() {
+    setState(() {
+      final bot = _bots.last;
+      if (bot.status == BotStatus.busy) {
+        final order = bot.order;
+        if (order != null) {
+          print("PUSHING BACK ORDER ${order.id} TO QUEUE..");
+          // Update order status to pending
+          // Add order back to the top of the list
+          // (but behind all VIP orders)
+          order.updateStatus(OrderStatus.pending);
+
+          final lastVIPOrder = _queue.lastWhereOrNull(
+            (order) => order.type == OrderType.vip,
+          );
+          final int idx = _queue.indexOf(lastVIPOrder ?? _queue.first);
+          _queue.insert(idx, order);
+        }
+      }
+      _bots.removeLast();
+    });
+  }
+
+  void _processOrder(Bot bot, Order order) {
     order.updateStatus(OrderStatus.inProgress);
     bot.addOrder(order);
 
@@ -45,55 +107,6 @@ class _HomeWidgetState extends State<HomeWidget> {
     });
   }
 
-  void _addOrder(OrderType type) {
-    setState(() {
-      final newOrder = Order(id: _orderCounter, type: type);
-      _incrementOrderCounter();
-      if (newOrder.type == OrderType.vip) {
-        _queue.insert(0, newOrder);
-      } else {
-        _queue.add(newOrder);
-      }
-      final availableBot = _bots.firstWhereOrNull(
-        (bot) => bot.status == BotStatus.idle,
-      );
-      if (availableBot != null) {
-        _addOrderToBot(availableBot, newOrder);
-      }
-    });
-  }
-
-  void _addBot() {
-    setState(() {
-      final newBot = Bot(id: _botCounter);
-      _incrementBotCounter();
-      final nextOrder = _queue
-          .firstWhereOrNull((order) => order.status == OrderStatus.pending);
-      if (nextOrder != null) {
-        _addOrderToBot(newBot, nextOrder);
-      }
-      _bots.add(newBot);
-    });
-  }
-
-  void _removeBot() {
-    setState(() {
-      final bot = _bots.last;
-      if (bot.status == BotStatus.busy) {
-        final order = bot.order;
-        if (order != null) {
-          print("PUSHING BACK ORDER ${order.id} TO QUEUE..");
-          // Update order status to pending
-          // Add order back to the list behind all VIP orders
-          order.updateStatus(OrderStatus.pending);
-          _queue.insert(
-              _queue.indexWhere((o) => o.type == OrderType.vip), order);
-        }
-      }
-      _bots.removeLast();
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -104,121 +117,27 @@ class _HomeWidgetState extends State<HomeWidget> {
         body: Center(
           child: Row(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    right: BorderSide(
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
-                  ),
-                ),
-                width: MediaQuery.of(context).size.width / 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "PENDING ORDERS",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 8),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: _queue.length,
-                        itemBuilder: (context, index) {
-                          final order = _queue[index];
-                          return ListTile(
-                            dense: true,
-                            title: Text(order.toString()),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    right: BorderSide(
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
-                  ),
-                ),
-                width: MediaQuery.of(context).size.width / 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "IN PROGRESS",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 8),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: _bots.length,
-                        itemBuilder: (context, index) {
-                          final bot = _bots[index];
-                          print("BOT.. ${bot.status}");
-                          print("BOT ORDER.. ${bot.order}");
-                          return BotWidget(
-                            id: bot.id,
-                            order: bot.order,
-                            onCompleteOrder: (order) {
-                              setState(() {
-                                _completedOrders.add(order);
-                                if (_queue.isNotEmpty) {
-                                  _addOrderToBot(bot, _queue.first);
-                                }
-                              });
-                            },
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    right: BorderSide(
-                      color: Theme.of(context).colorScheme.onSurface,
-                    ),
-                  ),
-                ),
-                width: MediaQuery.of(context).size.width / 3,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "COMPLETED ORDERS",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 8),
-                    Expanded(
-                      child: ListView.builder(
-                        itemCount: _completedOrders.length,
-                        itemBuilder: (context, index) {
-                          final order = _completedOrders[index];
-                          return ListTile(
-                            dense: true,
-                            title: Text(order.toString()),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              ListWidget(list: _queue, title: "PENDING ORDERS"),
+              ListWidget(
+                  list: _bots,
+                  title: "IN PROGRESS",
+                  itemBuilder: (Bot bot) {
+                    return BotWidget(
+                      id: bot.id,
+                      order: bot.order,
+                      onCompleteOrder: (order) => _onCompleteOrder(order, bot),
+                    );
+                  }),
+              ListWidget(list: _completedOrders, title: "COMPLETED ORDERS"),
             ],
           ),
         ),
         floatingActionButton: Controls(
           addNormalOrderCtrl: () {
-            _addOrder(OrderType.normal);
+            _addNewOrder(OrderType.normal);
           },
           addVipOrderCtrl: () {
-            _addOrder(OrderType.vip);
+            _addNewOrder(OrderType.vip);
           },
           addBotCtrl: () {
             _addBot();
---
+++ lib/ui/list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class ListWidget<T> extends StatelessWidget {
+  final List<T> list;
+  final String title;
+  final Widget Function(T)? itemBuilder;
+
+  const ListWidget({
+    super.key,
+    required this.list,
+    required this.title,
+    this.itemBuilder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+        ),
+      ),
+      width: MediaQuery.of(context).size.width / 3,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 8),
+          Expanded(
+            child: ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                final item = list[index];
+                return itemBuilder != null
+                    ? itemBuilder!(item)
+                    : ListTile(
+                        title: Text(item.toString()),
+                      );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
